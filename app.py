3fadeea437a71531f1de7c260696f3d42b894eb9
Zhou Liu <zhou@Zhous-MacBook-Pro.local>
2024-07-15 12:34:01 -0400

    fix datatype display error

--- app.py
+++ app.py
@@ -8,16 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # df = read_google_sheet()
     df = download_google_sheet(SPREADSHEET_ID, "copy.xlsx")
-    # return render_template('index.html', tables=[df.to_html(classes='data', header=True, index=False)])
     data = df.to_dict(orient='records')
     return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # read_google_sheet()
+    # read_google_sheet_by_api()
     # df = download_google_sheet(SPREADSHEET_ID, "copy.xlsx")
     # print(df.head())
     # print(extract_images_from_excel("copy.xlsx"))
