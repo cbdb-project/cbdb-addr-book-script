98402bae918c84c1ef90e2bcb1629027d382f172
Zhou Liu <zhou@Zhous-MBP.lan>
2024-07-22 16:04:21 -0400

    fix bug, add styles

--- docs/data.py
+++ docs/data.py
@@ -6,9 +6,14 @@ from config import SPREADSHEET_ID
 
 
 def prepare_data():
+    processed_data = []
     df = download_google_sheet(SPREADSHEET_ID, "copy.xlsx")
     data = df.to_dict(orient='records')
+
     for record in data:
+        # skip the empty lines between in boston and not in boston
+        if isinstance(record.get("Preferred First Name"), float) and math.isnan(record.get("Preferred First Name")):
+            continue
         for key, value in record.items():
             if isinstance(value, float) and math.isnan(value):
                 record[key] = ""
@@ -18,9 +23,13 @@ def prepare_data():
                 else:
                     record[key] = int(value)
             elif key == "Arrive/Depart":
-                record[key] = str(value)
+                if isinstance(value, float) and math.isnan(value):
+                    record[key] = ""
+                else:
+                    record[key] = str(value)
+        processed_data.append(record)
     with open('data.json', 'w') as f:
-        json.dump(data, f)
+        json.dump(processed_data, f)
 
 
 if __name__ == "__main__":
