ee5a6f7d2b1601951a38d7fcbe017e684f6323db
Zhou Liu <zhou@Zhous-MBP.lan>
2024-07-28 18:58:13 -0400

    debug

---
+++ docs/config.py
@@ -0,0 +1,11 @@
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+# The ID and range of a spreadsheet
+SPREADSHEET_ID = "19SUbSezEZ_ObEqfoNY3BDAM8z3cyBR-raql0Rs7_N3A"
+
+# i.e. sheet name
+RANGE_NAME = "current"
+
+DROP_COLUMN = ["Preferred First Name", "Cell", "VPA Signed", "Rotating mentor", "Group meeting presentation"]
+
